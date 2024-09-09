934098366ef7895a14eb104b3c6f68e680ebff5c
enbasto <enbo98@hotmail.com>
2024-09-09 14:09:56 -0500

    Controlladores de Euclides parte del proyecto

---
+++ Backend/Apimarket/Controllers/Message.Controller.cs
@@ -0,0 +1,28 @@
+﻿using APIADSO.Functions;
+using APIADSO.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIADSO.Controllers
+{
+    [ApiController]
+    [Route("/Api/[controller]")]
+    public class MessageController : Controller
+    {
+        // POST: Message/Edit/5
+        [HttpPost]
+        public IActionResult Mesagge(Persona persona)
+        {
+            try
+            {
+                var personaFunctions = new PersonaFunctions();
+                var messageSaludo = personaFunctions.Saludo(persona.Name);
+                return Ok(messageSaludo);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }    
+    }
+}
---
+++ Backend/Apimarket/Controllers/Number.Controller.cs
@@ -0,0 +1,78 @@
+﻿using APIADSO.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIADSO.Controllers
+{
+	[ApiController]
+	[Route("Api/[controller]")]
+    public class NumberController : Controller
+    {
+		[HttpPost("CreateNumber")]
+        public IActionResult CreateNumber(NumberModel number)
+        {
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, ex.Message.ToString());
+			}
+			return Ok();
+        }
+        [HttpGet("GetNumbers")]
+        public IActionResult GetNumbers()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            return Ok();
+        }
+
+        [HttpGet("GetNumber")]
+        public IActionResult GetNumber()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            return Ok();
+        }
+
+        [HttpPost("UpdateNumber")]
+        public IActionResult UpdateNumber(NumberModel number)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            return Ok();
+        }
+        [HttpDelete("DeleteNumber")]
+        public IActionResult DeleteNumber()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            return Ok();
+        }
+    }
+}
